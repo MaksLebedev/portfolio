9b5b184a810c143e2e5c7447b9b1224944c0f0d8
Maksim Lebedev <monk527@yandex.ru>
2024-06-24 00:53:37 +0500

    add new components: works and work

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,11 +3,13 @@ import styled from "styled-components";
 import { Logo } from "../../components/logo/Logo";
 import { Menu } from "../../components/menu/Menu";
 
+const items = ["Home", "Skills", "Works", "Testimony", "Contact"];
+
 export const Header = () => {
   return (
     <StyledHeader>
       <Logo />
-      <Menu />
+      <Menu menuItems={items} />
     </StyledHeader>
   );
 };
@@ -15,5 +17,5 @@ export const Header = () => {
 const StyledHeader = styled.header`
   background-color: #d4ffd3;
   display: flex;
-  justify-content: space-between
+  justify-content: space-between;
 `;
