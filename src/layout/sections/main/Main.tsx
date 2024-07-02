93ae9afd58507ac8b71aae8fa6e0b13b7400e33e
Maksim Lebedev <monk527@yandex.ru>
2024-07-02 20:12:15 +0500

    add style for main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,25 +2,30 @@ import React from "react";
 import styled from "styled-components";
 import photo from "../../../assets/images/photo.webp";
 import { FlexWrapper } from "../../../components/FlexWrapper";
+import { Container } from "../../../components/Container";
 
 export const Main = () => {
   return (
     <StyledMain>
-      <FlexWrapper align={"center"} justify={"space-around"}>
-        <div>
-          <span>Hi There</span>
-          <Name>I am Maksim Lebedev</Name>
-          <MainTitle>A Web developer</MainTitle>
-        </div>
-        <Photo src={photo} alt="photo_developer" />
-      </FlexWrapper>
+      <Container>
+        <FlexWrapper align={"center"} justify={"space-between"}>
+          <div>
+            <SmallText>Hi There</SmallText>
+            <Name>I am Maksim Lebedev</Name>
+            <MainTitle>A Web developer</MainTitle>
+          </div>
+          <Photo src={photo} alt="photo_developer" />
+        </FlexWrapper>
+      </Container>
     </StyledMain>
   );
 };
 
 const StyledMain = styled.section`
   min-height: 100vh;
-  /* background-color: #fff5e7; */
+  background-color: #fff5e7;
+  display: flex;
+  /* align-items: center; */
 `;
 
 const Photo = styled.img`
@@ -34,5 +39,16 @@ const MainTitle = styled.h1`
 `;
 
 const Name = styled.h2`
-  /* color: green; */
+  font-family: "Josefin Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 50px;
+  line-height: 50px;
+`;
+
+const SmallText = styled.h2`
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
 `;
