bb04f0c1965a1a49d2c103bba65af41e39a082a2
Maksim Lebedev <monk527@yandex.ru>
2024-07-07 23:37:46 +0500

    add adaptiv for header

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -10,4 +10,9 @@ export const theme = {
     placeholderColor: "#495057",
     imageHoverColor: "#1f1f20",
   },
+
+  media: {
+    tablet: 'screen and (max-width: 768px)',
+    mobile: 'screen and (max-width: 576px)',
+  }
 };
