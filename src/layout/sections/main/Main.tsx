09e32608fbed2b440475ce07e6764432f744c938
Maksim Lebedev <monk527@yandex.ru>
2024-07-08 20:05:24 +0500

    add adaptiv for main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,12 +4,13 @@ import photo from "../../../assets/images/photo.webp";
 import { FlexWrapper } from "../../../components/FlexWrapper";
 import { Container } from "../../../components/Container";
 import { theme } from "../../../styles/Theme";
+import { font } from "../../../styles/Common";
 
 export const Main = () => {
   return (
     <StyledMain>
       <Container>
-        <FlexWrapper align={"center"} justify={"space-between"}>
+        <FlexWrapper align={"center"} justify={"space-around"} wrap={"wrap"}>
           <div>
             <SmallText>Hi There</SmallText>
             <Name>
@@ -36,6 +37,7 @@ const StyledMain = styled.section`
 const PhotoWrapper = styled.div`
   position: relative;
   z-index: 1;
+  margin-top: 65px;
 
   &::before {
     content: "";
@@ -43,9 +45,16 @@ const PhotoWrapper = styled.div`
     width: 360px;
     height: 470px;
     border: 5px solid ${theme.colors.accent};
-    top: -24px;
+    top: -20px;
     right: -34px;
     z-index: -1;
+
+    @media ${theme.media.mobile} {
+      width: 314px;
+      height: 414px;
+      top: -20px;
+      right: -15px;
+    }
   }
 `;
 
@@ -53,24 +62,45 @@ const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
+  margin-right: 20px;
+
+  @media ${theme.media.mobile} {
+    width: 310px;
+    height: 380px;
+  }
 `;
 
 const MainTitle = styled.h1`
-  font-family: "Poppins", sans-serif;
+  ${font({
+    family: "'Poppins', sans-serif",
+    weight: 400,
+    Fmax: 27,
+    Fmin: 20,
+  })};
+
+  /* font-family: "Poppins", sans-serif;
   font-weight: 400;
-  font-size: 27px;
+  font-size: 27px; */
 `;
 
 const Name = styled.h2`
-  font-family: "Josefin Sans";
+  ${font({
+    family: "'Josefin Sans', sans-serif",
+    weight: 700,
+    Fmax: 50,
+    Fmin: 36,
+  })};
+
+  /* font-family: 'Josefin Sans, sans-serif';
   font-style: normal;
   font-weight: 700;
-  font-size: 50px;
+  font-size: 50px; */
   margin: 10px 0;
 
   span {
     position: relative;
     z-index: 0;
+    white-space: nowrap;
 
     &::before {
       content: "";
@@ -83,10 +113,21 @@ const Name = styled.h2`
       z-index: -1;
     }
   }
+
+  @media ${theme.media.mobile} {
+    margin: 15px 0 22px;
+  }
 `;
 
 const SmallText = styled.h2`
-  font-family: "Poppins", sans-serif;
+  ${font({
+    family: "'Poppins', sans-serif",
+    weight: 400,
+    Fmax: 14,
+    Fmin: 12,
+  })};
+
+  /* font-family: "Poppins", sans-serif;
   font-weight: 400;
-  font-size: 14px;
+  font-size: 14px; */
 `;
