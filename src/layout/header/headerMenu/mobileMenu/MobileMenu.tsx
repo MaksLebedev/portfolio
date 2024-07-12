4edbaaa690ab640670404d93d8f7841e63330505
Maksim Lebedev <monk527@yandex.ru>
2024-07-12 07:31:13 +0500

    add animation for burger

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,16 +1,28 @@
 import React from "react";
+import { useState } from "react";
 import { S } from "../HeaderMenu_Styles";
 import { Menu } from "../menu/Menu";
 
 export const MobileMenu: React.FC<{ menuItems: Array<string> }> = (props: {
   menuItems: Array<string>;
 }) => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const onBurgerBtnClick = () => {
+    setMenuIsOpen(!menuIsOpen);
+  };
+
   return (
     <S.MobileMenu>
-      <S.BurgerButton isOpen={false}>
+      <S.BurgerButton isOpen={menuIsOpen} onClick={onBurgerBtnClick}>
         <span></span>
       </S.BurgerButton>
-      <S.MobileMenuPopap isOpen={false}>
+      <S.MobileMenuPopap
+        isOpen={menuIsOpen}
+        onClick={() => {
+          setMenuIsOpen(false);
+        }}
+      >
         <Menu menuItems={props.menuItems} />
       </S.MobileMenuPopap>
     </S.MobileMenu>
